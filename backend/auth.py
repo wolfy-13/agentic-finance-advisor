31c3899667e48bba7ac494727319655433f90e79
mugiVs <vsmugi1803@gmail.com>
2026-03-20 09:46:13 +0530

    fix auth using sqlite

--- backend/auth.py
+++ backend/auth.py
@@ -1,12 +1,38 @@
-users = {}
+import sqlite3
+
+conn = sqlite3.connect("finance.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password TEXT
+)
+""")
+
+conn.commit()
+
 
 def register_user(username, password):
-    if username in users:
+    try:
+        cursor.execute(
+            "INSERT INTO users (username,password) VALUES (?,?)",
+            (username, password)
+        )
+        conn.commit()
+        return True
+    except:
         return False
-    users[username] = password
-    return True
+
 
 def login_user(username, password):
-    if username not in users:
-        return False
-    return users[username] == password
+    cursor.execute(
+        "SELECT * FROM users WHERE username=? AND password=?",
+        (username, password)
+    )
+    user = cursor.fetchone()
+
+    if user:
+        return True
+    return False
